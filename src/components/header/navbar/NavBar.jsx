e2492111c870ceb1758284242d42ae1b41facdf8
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2023-08-11 23:59:59 +0530

    Sayhi page is integrated

--- src/components/header/navbar/NavBar.jsx
+++ src/components/header/navbar/NavBar.jsx
@@ -6,9 +6,15 @@ import Logo from '../../../assets/pngegg.png'
 import { BrowserRouter, Link, useNavigate } from 'react-router-dom';
 import RealTimeClock from '../../clock/Clock';
 import { animated } from "@react-spring/web";
+import useSound from "use-sound";
+import ClickSound from '../../../assets/hambuger.wav'
+import ClockSound from '../../../assets/clock.mp3'
 
 const NavBar = () => {
   const navigate =useNavigate();
+  const[song, setSong] = useState(ClickSound)
+  const [play, {stop}] = useSound(song);
+ 
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   return (
     <div className="navbar-container  sticky  top-0 backdrop-blur-sm bg-white/30 w-full h-20 md:h-30 z-40 flex justify-between items-center box-border px-4 py-0 md:px-8 md:py-4 ">
@@ -21,7 +27,11 @@ const NavBar = () => {
         <img src={Logo} alt="brand-logo" width={100} height={100} />
       </div>
       {isMenuOpen && (
-        <animated.div className="nav-menu-expansion transform shadow-lg shadow-green-600/20 rotate-90 w-[250px] translate-x-[36vw] translate-y-40 md:rotate-0 md:translate-x-0 md:translate-y-0   md:w-[30%] bottom-0  h-10  rounded-full backdrop-blur-2xl flex  justify-between items-center p-2 px-4 bg-black  hover:md:w-[35%] hover:cursor-pointer  duration-500 ease-in-out ">
+        <animated.div
+          className="nav-menu-expansion transform shadow-lg shadow-green-600/20 rotate-90 w-[280px] translate-x-[36vw] translate-y-40 md:rotate-0 md:translate-x-0 md:translate-y-0   md:w-[30%] bottom-0  h-10  rounded-full backdrop-blur-2xl flex  justify-between items-center p-2 px-4 bg-black  hover:md:w-[35%] hover:cursor-pointer  duration-500 ease-in-out "
+          onMouseEnter={() => { setSong(ClockSound); play()}}
+          onMouseLeave={() => stop()}
+        >
           <RealTimeClock />
           <div className="separator text-gray-500 font-semibold">|</div>
           <div className="social-link-icon-wrapper flex justify-between w-1/2">
@@ -41,7 +51,7 @@ const NavBar = () => {
         className="nav-ham-burger w-fit"
         onClick={() => setIsMenuOpen(!isMenuOpen)}
       >
-        <button className="ham-burger h-24 ">
+        <button className="ham-burger h-24 " onClick={()=>{setSong(ClickSound); play();}}>
           {isMenuOpen ? (
             <PiHamburgerBold size={32} />
           ) : (
