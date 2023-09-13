7bafaf7972d6f85a78803466043616d4312f6290
Abhinav Rastogi <rastogi.abhinav8@gmail.com>
2023-09-13 14:19:27 +0530

    New Loader added

---
+++ src/components/loader/PotionLoader.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const PotionLoader = () => {
+  return (
+    <section className="scale-50 md:scale-100">
+      <div className="shadow "></div>
+      <div className="bowl ">
+        <div className="liquid "></div>
+      </div>
+    </section>
+  );
+}
+
+export default PotionLoader
